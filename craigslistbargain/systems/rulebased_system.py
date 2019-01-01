617919cb99e9165df00828571c44b02389faf0be
Mocchaso <saka.ksftn2718@gmail.com>
2019-01-01 15:59:59 +0900

    restrict system's role, seller in craigslistbargain(my_sotuken already has been implemented).

--- craigslistbargain/systems/rulebased_system.py
+++ craigslistbargain/systems/rulebased_system.py
@@ -3,5 +3,16 @@ from sessions.rulebased_session import RulebasedSession
 
 class RulebasedSystem(BaseRulebasedSystem):
     def _new_session(self, agent, kb, config=None):
-        return RulebasedSession.get_session(agent, kb, self.lexicon, config, self.generator, self.manager)
-
+        ### change parts
+        # restrict rulebased to seller.
+        #
+        # if kb.role == "buyer" -> AttributeError is occurred.
+        # AttributeError: 'NoneType' object has no attribute 'config'
+        # -> cocoa/web/main/backend.py, line 320
+        #
+        # However, system continue to select a scenario
+        #   until a scenario - rulebased:seller is selected.
+        # -> AttributeError is temporary problem.
+        if kb.role == "seller":
+            return RulebasedSession.get_session(agent, kb, self.lexicon, config, self.generator, self.manager)
+        ###
