49033876000872ae88b37f32d4504595c1508962
Mocchaso <saka.ksftn2718@gmail.com>
2019-01-14 02:53:28 +0900

    実験後のアンケートの設問変更に伴い、事前アンケートの設問も少々変更

--- my_sotuken/user_attributes_manager.py
+++ my_sotuken/user_attributes_manager.py
@@ -19,11 +19,11 @@ class UserAttributesManager:
         re-input when inputted str or float or int except 1 ~ 4
         """
         print("\n買い物をする際、あなたが最も重視する点は何ですか？")
-        print("1: 品質, 2: 価格, 3: 銘柄, 4: Google検索の件数（情報量）\n")
+        print("1: 品質, 2: 価格, 3: 銘柄\n")
         while True:
             try:
-                UserAttributesManager.answer = int(raw_input("回答（1～4） -> ")) # cocoaの環境が2.7なのでraw_input
-                if UserAttributesManager.answer < 1 or UserAttributesManager.answer > 4:
+                UserAttributesManager.answer = int(raw_input("回答（1～3） -> ")) # cocoaの環境が2.7なのでraw_input
+                if UserAttributesManager.answer < 1 or UserAttributesManager.answer > 3:
                     raise AnswerRangeError()
             except (ValueError, AnswerRangeError) as e:
                 print(e)
